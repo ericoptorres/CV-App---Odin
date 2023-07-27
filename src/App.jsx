1541c39073e1ea81a2c3cbf24f46b80b50b7a342
erico <erico_pt@hotmail.com>
2023-07-27 15:17:29 -0300

    Setting structure

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,15 @@ function App() {
 
   return (
     <>
-      
-      
+      <div className='all'>
+        <div className='info'>
+          <div>General Info</div>
+          <div>Educational Experiences</div>
+          <div>Practical Experiences</div>
+      </div>
+
+      <section>Main</section>
+      </div>
     </>
   )
 }
